e6420ece5ab4cde7bf4debd305c36ac5c70c5ccf
Matthew Grasinger <grasingerm@gmail.com>
2014-05-09 18:07:22 -0400

    poor attempts at animation

--- src/animate3.c
+++ src/animate3.c
@@ -1,36 +1,35 @@
 #include <gtk/gtk.h>
-#include <cairo.h>
 #include <unistd.h>
 #include <pthread.h>
 
-//the global csurf that will serve as our buffer
-static cairo_surface_t *csurf = NULL;
+//the global pixmap that will serve as our buffer
+static GdkPixmap *pixmap = NULL;
 
 gboolean on_window_configure_event(GtkWidget * da, GdkEventConfigure * event, gpointer user_data){
     static int oldw = 0;
     static int oldh = 0;
-    //make our selves a properly sized csurf if our window has been resized
+    //make our selves a properly sized pixmap if our window has been resized
     if (oldw != event->width || oldh != event->height){
-        //create our new csurf with the correct size.
-        cairo_surface_t *tmpcsurf = cairo_image_surface_create(CAIRO_FORMAT_A1, event->width,  event->height);
-        //copy the contents of the old csurf to the new csurf.  This keeps ugly uninitialized
-        //csurfs from being painted upon resize
+        //create our new pixmap with the correct size.
+        GdkPixmap *tmppixmap = gdk_pixmap_new(da->window, event->width,  event->height, -1);
+        //copy the contents of the old pixmap to the new pixmap.  This keeps ugly uninitialized
+        //pixmaps from being painted upon resize
         int minw = oldw, minh = oldh;
         if( event->width < minw ){ minw =  event->width; }
         if( event->height < minh ){ minh =  event->height; }
-        gdk_draw_drawable(tmpcsurf, da->style->fg_gc[GTK_WIDGET_STATE(da)], csurf, 0, 0, 0, 0, minw, minh);
-        //we're done with our old csurf, so we can get rid of it and replace it with our properly-sized one.
-        cairo_surface_destroy(csurf);
-        csurf = tmpcsurf;
+        gdk_draw_drawable(tmppixmap, da->style->fg_gc[GTK_WIDGET_STATE(da)], pixmap, 0, 0, 0, 0, minw, minh);
+        //we're done with our old pixmap, so we can get rid of it and replace it with our properly-sized one.
+        g_object_unref(pixmap); 
+        pixmap = tmppixmap;
     }
     oldw = event->width;
     oldh = event->height;
     return TRUE;
 }
 
-gboolean on_draw_event(GtkWidget * da, GdkEventExpose * event, gpointer user_data){
+gboolean on_window_expose_event(GtkWidget * da, GdkEventExpose * event, gpointer user_data){
     gdk_draw_drawable(da->window,
-        da->style->fg_gc[GTK_WIDGET_STATE(da)], csurf,
+        da->style->fg_gc[GTK_WIDGET_STATE(da)], pixmap,
         // Only copy the area that was exposed.
         event->area.x, event->area.y,
         event->area.x, event->area.y,
@@ -48,7 +47,7 @@ void *do_draw(void *ptr){
 
     int width, height;
     gdk_threads_enter();
-    gdk_drawable_get_size(csurf, &width, &height);
+    gdk_drawable_get_size(pixmap, &width, &height);
     gdk_threads_leave();
 
     //create a gtk-independant surface to draw on
@@ -64,7 +63,7 @@ void *do_draw(void *ptr){
     for(k=0; k<100; ++k){   //lets just redraw lots of times to use a lot of proc power
         for(j=0; j < 1000; ++j){
             cairo_set_source_rgb (cr, (double)j/1000.0, (double)j/1000.0, 1.0 - (double)j/1000.0);
-            cairo_move_to(cr, i,j/2);
+            cairo_move_to(cr, i,j/2); 
             cairo_line_to(cr, i+100,j/2);
             cairo_stroke(cr);
         }
@@ -72,14 +71,14 @@ void *do_draw(void *ptr){
     cairo_destroy(cr);
 
 
-    //When dealing with gdkcsurf's, we need to make sure not to
+    //When dealing with gdkPixmap's, we need to make sure not to
     //access them from outside gtk_main().
     gdk_threads_enter();
 
-    cairo_t *cr_csurf = gdk_cairo_create(csurf);
-    cairo_set_source_surface (cr_csurf, cst, 0, 0);
-    cairo_paint(cr_csurf);
-    cairo_destroy(cr_csurf);
+    cairo_t *cr_pixmap = gdk_cairo_create(pixmap);
+    cairo_set_source_surface (cr_pixmap, cst, 0, 0);
+    cairo_paint(cr_pixmap);
+    cairo_destroy(cr_pixmap);
 
     gdk_threads_leave();
 
@@ -98,8 +97,8 @@ gboolean timer_exe(GtkWidget * window){
     //we don't run into the usual multithreading issues
     int drawing_status = g_atomic_int_get(&currently_drawing);
 
-    //if we are not currently drawing anything, launch a thread to
-    //update our csurf
+    //if we are not currently drawing anything, launch a thread to 
+    //update our pixmap
     if(drawing_status == 0){
         static pthread_t thread_info;
         int  iret;
@@ -111,7 +110,7 @@ gboolean timer_exe(GtkWidget * window){
 
     //tell our window it is time to draw our animation.
     int width, height;
-    gdk_drawable_get_size(csurf, &width, &height);
+    gdk_drawable_get_size(pixmap, &width, &height);
     gtk_widget_queue_draw_area(window, 0, 0, width, height);
 
     first_execution = FALSE;
@@ -134,16 +133,16 @@ int main (int argc, char *argv[]){
 
     GtkWidget *window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
     g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
-    g_signal_connect(G_OBJECT(window), "draw", G_CALLBACK(on_draw_event), NULL);
+    g_signal_connect(G_OBJECT(window), "expose_event", G_CALLBACK(on_window_expose_event), NULL);
     g_signal_connect(G_OBJECT(window), "configure_event", G_CALLBACK(on_window_configure_event), NULL);
 
-    //this must be done before we define our csurf so that it can reference
+    //this must be done before we define our pixmap so that it can reference
     //the colour depth and such
     gtk_widget_show_all(window);
 
-    //set up our csurf so it is ready for drawing
-    csurf = gdk_csurf_new(window->window,500,500,-1);
-    //because we will be painting our csurf manually during expose events
+    //set up our pixmap so it is ready for drawing
+    pixmap = gdk_pixmap_new(window->window,500,500,-1);
+    //because we will be painting our pixmap manually during expose events
     //we can turn off gtk's automatic painting and double buffering routines.
     gtk_widget_set_app_paintable(window, TRUE);
     gtk_widget_set_double_buffered(window, FALSE);
@@ -156,4 +155,3 @@ int main (int argc, char *argv[]){
 
     return 0;
 }
-
